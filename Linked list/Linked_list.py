8239945a5dfe3766625b8dab48a6549dcfbf0069
Py-Ashraful <ashrafulislam180@gmail.com>
2021-03-02 22:43:15 +0600

    Linked list

--- Linked list/Linked_list.py
+++ Linked list/Linked_list.py
@@ -7,3 +7,9 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
+
+    def printList(self):
+        current_node = self.head
+        while current_node:
+            print(current_node.data, "-->", end=' ')
+            current_node = current_node.next
