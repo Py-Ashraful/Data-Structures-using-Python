806969c53455f8ea204103b3b098280827f010d6
Py-Ashraful <ashrafulislam180@gmail.com>
2021-03-07 21:39:50 +0600

    Binary Tree

---
+++ Tree/binary_tree.py
@@ -0,0 +1,28 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+class BinaryTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, data):
+        if self.root:
+            self.root = Node(data)
+        else:
+            self._insert(data, self.root)
+
+    def _insert(self, data, current_node):
+        if data < current_node.data:
+            if current_node.left == None:
+                current_node.left = Node(data)
+            else:
+                self._insert(data, current_node.left)
+        elif data > current_node.data:
+            if current_node.right == None:
+                current_node.right = Node(data)
+            else:
+                self._insert(data, current_node.right)
